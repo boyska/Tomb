513d119a9084fa9e5f6117fe31a0331ed449ed93
boyska <piuttosto@logorroici.org>
2012-08-31 13:01:06 +0200

    Better testall.sh
    better output, colors, summary

--- src/test/testall.sh
+++ src/test/testall.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env zsh
 
 source utils.sh
-if [[ -z $output ]]; then
-	output=/dev/null
-fi
 if [[ -z $sudo_pwd ]]; then
 	echo "WARNING: sudo_pwd is probably needed by some test"
 fi
 rm /tmp/tomb_test_errorlog -f &> /dev/null
+has_err=0
+autoload colors
+colors
 for t in *.test.sh; do
-	sudo_pwd=$sudo_pwd source $t 3>> /tmp/tomb_test_errorlog 4>> /tmp/tomb_test_fulllog
+	echo -n "$fg[yellow]$t start test... $fg[default]"
+	sudo_pwd=$sudo_pwd source $t 3> /tmp/tomb_test_errorlog 4> /tmp/tomb_test_fulllog
 	ret=$?
-done
-
-if [[ `stat -c '%s' /tmp/tomb_test_errorlog` == 0 ]]; then
-	echo "No errors!"
-else
-	< /tmp/tomb_test_errorlog
-	rm /tmp/tomb_test_errorlog
+	if [[ `stat -c '%s' /tmp/tomb_test_errorlog` == 0 ]]; then
+		echo "$fg[green] OK$fg[default]"
+	else
+		echo "$fg[red] ERRORS$fg[default]"
+		< /tmp/tomb_test_errorlog
+		rm /tmp/tomb_test_errorlog
 #TODO: make it optional!
-	echo "\n--- Full log ---\n"
-	< /tmp/tomb_test_fulllog
-	rm /tmp/tomb_test_fulllog
-	exit 1
-fi
+echo "\n--- Full log (for $t) ---\n"
+		< /tmp/tomb_test_fulllog
+		rm /tmp/tomb_test_fulllog
+		has_err=1
+	fi
+done
+exit $has_err
+
 
